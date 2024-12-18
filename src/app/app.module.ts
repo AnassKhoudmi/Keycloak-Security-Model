d27fa40ea1905507c393c602eddfe480a48cf295
AnassKHOUDMI <a_khoudmi@etu.enset-media.ac.ma>
2024-12-18 19:37:57 +0100

    Version1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,45 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './ui/products/products.component';
 import { CustomersComponent } from './ui/customers/customers.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClientModule, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'bdcc-realm',
+        clientId: 'ecom-client-ang'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductsComponent,
-    CustomersComponent
+        AppComponent,
+        ProductsComponent,
+        CustomersComponent
   ],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    KeycloakAngularModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {provide : APP_INITIALIZER, useFactory: initializeKeycloak, multi: true, deps: [KeycloakService]}
+  ]
 })
+
 export class AppModule { }
